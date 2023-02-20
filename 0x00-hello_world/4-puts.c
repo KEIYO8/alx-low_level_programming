5909e24b19407fd987173c592dca3982c94e95b4
keiyo8 <gk8.africa@gmail.com>
2023-02-20 04:08:32 -0800

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - print the code using puts
- *
- * Return: 0 if success
+ * main - main entry point
+ * return : always 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilinqual puzzle");
 	return (0);
 }
+
